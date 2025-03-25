66a2b4f7763d8c798ad94e635c4fdd9580d643cd
B-MO56 <153782269+bkmorris2456@users.noreply.github.com>
2025-03-25 12:00:10 -0400

    created signup skeleton, added additional structure to login and signup

--- gedara/frontend/pages/login.js
+++ gedara/frontend/pages/login.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, StyleSheet, Image, TouchableOpacity, useColorScheme, TextInput } from 'react-native';
+import { Provider as PaperProvider, Surface, Button } from 'react-native-paper';
 import Template from '../pages/template';
 import Card from '../components/card';
 
@@ -11,32 +12,51 @@ export default function Login({ navigation }) {
     return (
         <Template navigation={navigation}>
 
-        <View style={styles.intro_text}>
-            <Text>Welcome to Gedara!</Text>
-            <Text>Your official tool for managing your home!</Text>
-        </View>
-
-        <Card style={[StyleSheet.login_container]} width="100%" height={450}>
-
-            <View style={styles.info_organizer}>
-
-                <Text style={styles.text_color}>Account Login</Text>
-                <TextInput style={styles.input} onChangeText={onChangeEmail} value={email} />
-                <TextInput style={styles.input} onChangeText={onChangePassword} value={password} />
-
-
+            <View style={styles.intro_text}>
+                <Text style={styles.intro_text}>Welcome to Gedara</Text>
             </View>
 
-        </Card>
+            <Card style={[StyleSheet.login_container]} width="100%" height={450}>
+
+                <View style={styles.info_organizer}>
+
+                    <Text style={styles.login_text}>Login</Text>
+                    <TextInput style={styles.input} onChangeText={onChangeEmail} value={email} />
+                    <TextInput style={styles.input} onChangeText={onChangePassword} value={password} />
+
+                    <Button
+                    mode="contained"
+                    onPress={() => console.log("Save pressed")}
+                    style={styles.save}
+                    >
+                        Save
+                    </Button>
+
+                    <Button
+                        mode="contained"
+                        onPress={() => navigation.navigate('Signup')}
+                        style={styles.signup_button}
+                    >
+                        Sign Up
+                    </Button>
+
+
+                </View>
+
+            </Card>
+            
         </Template>
     );
 };
 
 const styles = StyleSheet.create({
     intro_text: {
+        display: 'flex',
         padding: 20,
         marginBottom: 10,
-        color: "#fff"
+        color: "#fff",
+        alignItems: 'center',
+        fontSize: 24,
     },
     login_container: {
         flexDirection: 'column',
@@ -50,14 +70,35 @@ const styles = StyleSheet.create({
     },
     input: {
         height: 40,
-        margin: 12,
+        marginVertical: 20,
         borderWidth: 1,
         borderColor: "#fff",
         padding: 10,
         color: '#fff',
         width: '275',
     },
-    text_color: {
+    login_text: {
+        display: 'flex',
         color: '#fff',
+        fontSize: 24,
+        marginBottom: 20,
+        justifyContent: 'center',
+    },
+    save: {
+        width: "100%",
+        height: 40,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "green",
+        marginTop: 20,
+    },
+    signup_button: {
+        width: "100%",
+        height: 40,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 20,
     },
 });
--- gedara/frontend/pages/signup.js
+++ gedara/frontend/pages/signup.js
@@ -1,14 +1,106 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity, useColorScheme, TextInput } from 'react-native';
+import { Provider as PaperProvider, Surface, Button } from 'react-native-paper';
 import Template from '../pages/template';
 import Card from '../components/card';
 
-export default function Signup({ navigation }) {
-  return (
-    <Template navigation={navigation}>
-      <Card width="100%" height={200}>
-        <Text>Signup Page</Text>
-      </Card>
-    </Template>
-  );
-}
+export default function Login({ navigation }) {
+
+    const [email, onChangeEmail] = React.useState('Email');
+    const [password, onChangePassword] = React.useState('Password');
+    const [name, onChangeName] = React.useState('Name');
+
+    return (
+        <Template navigation={navigation}>
+
+            <View style={styles.intro_text}>
+                <Text style={styles.intro_text}>Welcome to Gedara</Text>
+            </View>
+
+            <Card style={[StyleSheet.login_container]} width="100%" height={500}>
+
+                <View style={styles.info_organizer}>
+
+                    <Text style={styles.login_text}>Sign Up!</Text>
+                    <TextInput style={styles.input} onChangeText={onChangeName} value={name} />
+                    <TextInput style={styles.input} onChangeText={onChangeEmail} value={email} />
+                    <TextInput style={styles.input} onChangeText={onChangePassword} value={password} />
+
+                    <Button
+                    mode="contained"
+                    onPress={() => console.log("Save pressed")}
+                    style={styles.save}
+                    >
+                        Sign Up
+                    </Button>
+
+                    <Button
+                        mode="contained"
+                        onPress={() => console.log("Login Pressed")}
+                        style={styles.signup_button}
+                    >
+                        Back to Login
+                    </Button>
+
+
+                </View>
+
+            </Card>
+            
+        </Template>
+    );
+};
+
+const styles = StyleSheet.create({
+    intro_text: {
+        display: 'flex',
+        padding: 20,
+        marginBottom: 10,
+        color: "#fff",
+        alignItems: 'center',
+        fontSize: 24,
+    },
+    login_container: {
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    info_organizer: {
+        padding: 20,
+        marginBottom: 25,
+        color: '#fff',
+    },
+    input: {
+        height: 40,
+        marginVertical: 20,
+        borderWidth: 1,
+        borderColor: "#fff",
+        padding: 10,
+        color: '#fff',
+        width: '275',
+    },
+    login_text: {
+        display: 'flex',
+        color: '#fff',
+        fontSize: 24,
+        marginBottom: 20,
+        justifyContent: 'center',
+    },
+    save: {
+        width: "100%",
+        height: 40,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "green",
+        marginTop: 20,
+    },
+    signup_button: {
+        width: "100%",
+        height: 40,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 20,
+    },
+});
